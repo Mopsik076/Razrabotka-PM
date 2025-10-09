62d307459e1fb8bbe57f0fd8eb2af6225701f8f9
Dmitriy <97390134+Mopsik076@users.noreply.github.com>
2025-10-09 17:39:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function id (matrix, target) {
+    for (let i = 0; i < matrix.length; i++){
+        const Neko = matrix[i];
+        const Nekoid = Neko.indexOf(target)
+        if (Nekoid !== -1) {
+            return [i,Nekoid]
+        };
+    };
+};
+
+
+a = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+alert(id(a, 8));
